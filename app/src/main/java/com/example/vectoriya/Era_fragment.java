790ccdcc57fa96606c4cbf6ebdc466da017cc3c1
Angelina <titovagela6@gmail.com>
2024-11-27 18:28:22 +0300

    Four

--- app/src/main/java/com/example/vectoriya/Era_fragment.java
+++ app/src/main/java/com/example/vectoriya/Era_fragment.java
@@ -2,6 +2,7 @@ package com.example.vectoriya;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,31 +10,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Era_fragment extends Fragment {
-    int count;
-    String[] posts_person;
-    Person people;
-    TextView text_post;
-    TextView text_rat;
     public Era_fragment(){
         super(R.layout.era_fragment);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        text_post = view.findViewById(R.id.text_position_person);// текст звания пользователя в игре
-        text_rat = view.findViewById(R.id.text_rating);
-        people = new Person("Nikita");//создаем человека
-        count = people.getRating();
-        posts_person = getResources().getStringArray(R.array.post_person);// массив всех 20 званий
-        change_count();
-        people.setRating(5300);
-        change_count();
+        Button btn_exit = findViewById(R.id.btn_exit_year_war);
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
-    findViewById(R.id.myButtom).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            onBackPressed();
-        }
-    });
 
 }
